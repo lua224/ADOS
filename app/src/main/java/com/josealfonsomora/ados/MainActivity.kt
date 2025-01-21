96e0deac21fc4e7dce6d5cdeaf6bab2a07897fd5
Jose Alfonso Mora <josealfonso@fpfomento.com>
2025-01-21 22:38:14 +0100

    Add image, text, list, appbar and scafold

--- app/src/main/java/com/josealfonsomora/ados/MainActivity.kt
+++ app/src/main/java/com/josealfonsomora/ados/MainActivity.kt
@@ -1,16 +1,29 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.josealfonsomora.ados
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.josealfonsomora.ados.ui.theme.ADOSTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,30 +32,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ADOSTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainContent()
+
             }
+        }
+    }
+
+}
+
+@Composable
+private fun MainContent() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text("ADOS") }) }) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Image(
+                painter = painterResource(id = R.drawable.ados), // Replace with your local image resource name
+                contentDescription = "Local JPG Image",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            ListOfDonationPlaces()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun ListOfDonationPlaces() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("List of places to donate", style = MaterialTheme.typography.titleLarge)
+        val itemsList = listOf(
+            "A Coruña", "Pontevedra", "Vigo", "Lugo", "Ourense"
+        )
+        LazyColumn(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            items(itemsList.sorted()) {
+                Text(it)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ADOSTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
